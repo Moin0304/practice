4c86f345c4b0d76530b811718e6943244ab8721c
clavit venky <clavitvenky@gmail.com>
2023-03-07 10:27:33 +0530

    changed

--- print_pattern.py
+++ print_pattern.py
@@ -1,14 +1,5 @@
 import time
 
-# while True:
-#     print("**********")
-#     print("********")
-#     print("******")
-#     print("***")
-#     print("*")
-#     for i in range(60*60*2):
-#         # wait for 2 hours (60 seconds * 60 minutes * 2)
-#         time.sleep(1)
 while True:
     starttime= int(time.time())
     # print(starttime)
